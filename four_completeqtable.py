09dd297378266c35bea982d3cd0212609f548cb9
Madhavan B S <112371902+MadhavanBS@users.noreply.github.com>
2024-08-11 18:39:10 +0530

    50000 iterations

--- four_completeqtable.py
+++ four_completeqtable.py
@@ -2,13 +2,14 @@ import gymnasium as gym
 import numpy as np
 
 
-env = gym.make('MountainCar-v0', render_mode="human")
+env = gym.make('MountainCar-v0', render_mode=None)
 env.reset()
 done = False
 
 learning_rate = 0.1
 discount = 0.95
-episodes = 25_000
+episodes = 50_000
+
 
 discrete_observation_space_size = [20] * len(env.observation_space.high)
 discrete_observation_space_window_size = (env.observation_space.high-env.observation_space.low)/discrete_observation_space_size
@@ -22,12 +23,21 @@ def continous_to_discrete(state):
 discrete_state = continous_to_discrete(env.reset()[0])
 
 for episode in range(episodes):
+
+    if episode%10_000 == 0:
+        render_mode="human"
+    else:
+        render_mode=None
+
+    env = gym.make('MountainCar-v0', render_mode=render_mode)
+    env.reset()
+
     while not done: 
         action = np.argmax(q_table[discrete_state])
         new_state, reward, termination, truncation, _ = env.step(action) 
         new_discrete_state = continous_to_discrete(new_state)
         done = termination or truncation
-        # print(f"new_state: {new_state}\nreward: {reward}\ndone: {done}\n")
+        #print(f"new_state: {new_state}\nreward: {reward}\ndone: {done}\n")
         if not done:
             max_future_q = np.max(q_table[new_discrete_state])
             current_q = q_table[discrete_state+(action,)]
@@ -37,6 +47,12 @@ for episode in range(episodes):
             q_table[discrete_state+(action,)] = 0
 
         discrete_state = new_discrete_state
+
+    if episode%10_000 == 0:
+        print(episode)
+        print(f"new_state: {new_state}\nreward: {reward}\ndone: {done}\n")
+    
+    done = False
 
 print("done")
 

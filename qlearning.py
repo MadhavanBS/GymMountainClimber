e3e787c443dcca956a99e889bf57ab61547c9ecd
Madhavan B S <112371902+MadhavanBS@users.noreply.github.com>
2024-08-11 13:31:31 +0530

    removed comments

--- qlearning.py
+++ qlearning.py
@@ -6,13 +6,11 @@ done = False
 print(env.observation_space.high)
 print(env.observation_space.low)
 print(env.action_space.n)
-'''
+
 while not done: 
     action = 2
     new_state, reward, done, _, _ = env.step(action)
-    # new_state, *others = env.step(action)
-    # print(f"new_state: {new_state}\nreward: {reward}\ndone: {done}\n")
+    print(f"new_state: {new_state}\nreward: {reward}\ndone: {done}\n")
     env.render()
 
 env.close()
-'''

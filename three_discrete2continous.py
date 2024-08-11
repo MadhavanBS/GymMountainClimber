60c2c53dd235d570148e46e64de4d92087d52032
Madhavan B S <112371902+MadhavanBS@users.noreply.github.com>
2024-08-11 14:10:40 +0530

    renaming files

--- three_discrete2continous.py
+++ three_discrete2continous.py
@@ -5,11 +5,26 @@ env = gym.make('MountainCar-v0', render_mode="human")
 env.reset()
 done = False
 
+learning_rate = 0.1
+discount = 0.95
+episodes = 25_000
 
 discrete_observation_space_size = [20] * len(env.observation_space.high)
 discrete_observation_space_window_size = (env.observation_space.high-env.observation_space.low)/discrete_observation_space_size
 
 q_table = np.random.uniform(low=-2, high=0, size=discrete_observation_space_size+[env.action_space.n])
+
+def continous_to_discrete(state):
+    discrete_state = (state-env.observation_space.low)/discrete_observation_space_size
+    return tuple(discrete_state.astype(int))
+
+
+discrete_state = continous_to_discrete(env.reset()[0])
+
+print(discrete_state)
+
+print(q_table[discrete_state])
+print(np.argmax(q_table[discrete_state]))
 
 '''
 while not done: 

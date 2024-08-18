6cf5b1f161b6bb19ae99596808a77df77e17f185
Madhavan B S <sakthiushadevi@gmail.com>
2024-08-18 13:41:24 +0530

    dqlearning start

---
+++ DQLearning/one_start.py
@@ -0,0 +1,136 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2d, MaxPooling2D, Activation 
+from keras.callbacks import TensorBoard
+from keras.optimizers import Adam
+from collections import deque
+
+import numpy as np
+import time
+
+import random
+
+replay_memory_size = 50_000
+min_replay_memory_size = 1_000
+model_name = "256x2"
+mini_batch_size = 64
+discount = 0.99
+
+update_target_every = 5
+
+# Own Tensorboard class
+class ModifiedTensorBoard(TensorBoard):
+
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.FileWriter(self.log_dir)
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # Overrided, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Overrided
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    # Overrided, so won't close writer
+    def on_train_end(self, _):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
+
+
+
+class DQNAgent:
+
+    def __init__(self):
+        # gets trained every step
+        self.model = self.create_model()
+
+        # we predict against this model every step
+        self.target_model = self.create_model()
+        self.target_model.set_weights(self.model.get_weights())
+
+        self.replay_memory = deque(maxlen = replay_memory_size)
+
+        self.tensorboard = ModifiedTensorBoard(log_dir=f"logs/{model_name}-{int(time.time())}") 
+
+        self.target_update_counter = 0
+
+    def create_model(self):
+        model = Sequential()
+        model.add(Conv2D(256, (3,3), input_shape=observation_space_values))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(2,2))
+        model.add(Dropout(0.2))
+
+        model.add(Conv2D(256, (3,3)))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(2,2))
+        model.add(Dropout(0.2))
+
+        model.add(Flatten())
+        model.add(Dense(64))
+
+        model.add(Dense(activation_space, activation="linear"))
+        model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=["accuracy"])
+        return model
+
+def update_replay_memory(self, transition):
+    self.replay_memory.append(transition)
+
+def get_qs(self, terminal_state, step):
+    return seld.model_predict(np.array(state).reshape(-1, *state.shape)/255)[0]
+
+def train(self, terminal_state, step):
+    if len(self.replay_memory) < min_replay_memory_size:
+        return
+
+    mini_batch = random.sample(self.replay_memory, mini_batch_size)
+
+    current_states = np.array([transition[0] for transition in mini_batch])/ 255
+
+    current_qs_list = self.model.predict(current_states)
+
+    new_current_states = np.array([transition[3] for transition in mini_batch])/255
+
+    future_qs_list = self.target_model.predict(new_current_states)
+
+    X = []
+    y = []
+    
+    for index, (current_state, action, reward, new_current_state, done) in enumerate(mini_batch):
+        if not done:
+            max_future_q = np.max(future_qs_list{index})
+            new_q = reward + discount * max_future_q
+        else:
+            new_q = reward
+
+        current_qs = current_qs_list[index]
+
+        current_qs[action] = new_q
+
+        X.append(current_state)
+        y.append(current_qs)
+
+    self.model_fit(np.array(X)/255, np.array(y), batch_size=mini_batch_size,
+     verbose=0, shuffle=False, callbacks=[self.tensorboard] if terminal_state else None)
+
+    if terminal_state:
+        self.target_update_counter+=1
+
+    if self.target_update_counter > update_target_every:
+        self.target_model.set_weights(self.model.get_weights())
+        self.target_update_counter = 
+
+
